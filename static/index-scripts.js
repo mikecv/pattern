adfc3f6ccecc6322b6c6e354d5b732a65ca098e0
mdc <galacticwingcommander@gmail.com>
2025-01-04 17:26:20 +0800

    Added fixed zoom buttons.

--- static/index-scripts.js
+++ static/index-scripts.js
@@ -189,3 +189,52 @@ fractalImage.addEventListener("click", (event) => {
         alert("Failed to recentre and generate fractal.");
     })
 });
+
+const times2Button = document.getElementById("times2Button");
+const times3Button = document.getElementById("times3Button");
+const times5Button = document.getElementById("times5Button");
+const times10Button = document.getElementById("times10Button");
+
+// Listener for fractal zoom x 2 button pressed.
+document.getElementById('times2Button').addEventListener('click', () => {
+
+    // Get current value of pixel division and divide by factor.
+    // Dividing pixel division will give corresponding zoom be that factor.
+    const value5 = parseFloat(document.getElementById('init_pt_div').value);
+    const newZoomValue = value5 / 2.0;
+    // Update the pixel division.
+    document.getElementById('init_pt_div').value = newZoomValue;
+});
+
+// Listener for fractal zoom x 3 button pressed.
+document.getElementById('times3Button').addEventListener('click', () => {
+
+    // Get current value of pixel division and divide by factor.
+    // Dividing pixel division will give corresponding zoom be that factor.
+    const value5 = parseFloat(document.getElementById('init_pt_div').value);
+    const newZoomValue = value5 / 3.0;
+    // Update the pixel division.
+    document.getElementById('init_pt_div').value = newZoomValue;
+});
+
+// Listener for fractal zoom x 5 button pressed.
+document.getElementById('times5Button').addEventListener('click', () => {
+
+    // Get current value of pixel division and divide by factor.
+    // Dividing pixel division will give corresponding zoom be that factor.
+    const value5 = parseFloat(document.getElementById('init_pt_div').value);
+    const newZoomValue = value5 / 5.0;
+    // Update the pixel division.
+    document.getElementById('init_pt_div').value = newZoomValue;
+});
+
+// Listener for fractal zoom x 10 button pressed.
+document.getElementById('times10Button').addEventListener('click', () => {
+
+    // Get current value of pixel division and divide by factor.
+    // Dividing pixel division will give corresponding zoom be that factor.
+    const value5 = parseFloat(document.getElementById('init_pt_div').value);
+    const newZoomValue = value5 / 10.0;
+    // Update the pixel division.
+    document.getElementById('init_pt_div').value = newZoomValue;
+});
